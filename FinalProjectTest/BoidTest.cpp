89665102ea4435152705b5b48162f16aad8b0e65
Bex Edmondson <bexedmondson@gmail.com>
2016-03-01 16:38:47 +0000

    added test project

---
+++ FinalProjectTest/BoidTest.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FinalProjectTest
+{		
+	TEST_CLASS(BoidTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
