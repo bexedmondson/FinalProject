d29dcbb5d4713d19c7140a3700f1eb132567a1c8
Bex Edmondson <bexedmondson@gmail.com>
2016-04-16 15:18:39 +0100

    added boid test classes

---
+++ Source/FinalProject/TestBoid.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FinalProject.h"
+#include "TestBoid.h"
+
+ATestBoid::ATestBoid(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
+
+void ATestBoid::BeginPlay()
+{
+
+}
+
+// Called every frame
+void ATestBoid::Tick(float DeltaTime)
+{
+
+}
+
+
---
+++ Source/FinalProject/TestBoid.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Boid.h"
+#include "TestBoid.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALPROJECT_API ATestBoid : public ABoid
+{
+	GENERATED_BODY()
+
+public:
+	ATestBoid(const FObjectInitializer& ObjectInitializer);
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+	
+	
+	
+};
